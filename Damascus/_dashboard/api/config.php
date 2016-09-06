9820b9319cd17e7c24661f230639323d79ccecfe
Chad McElwain <Forge@Macociraptor.local>
2016-09-06 12:27:20 -0500

    Initial push

---
+++ Damascus/_dashboard/api/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    //$host   = 'apollomeddev.cniwh0dczhs3.us-west-2.rds.amazonaws.com';
+    //$name   = 'ApplDev';
+    //$pass   = 'HulkSmash#1';
+    //$data   = 'users';
+    //$port   = 3306;
+    
+    $host   = 'localhost';
+    $name   = 'root';
+    $pass   = 'root';
+    $data   = 'soradev';
+    $port   = 3306;
+
+
+?>
